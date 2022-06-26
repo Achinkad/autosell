8297bdc1d88b8c957c8e9928012033790edaf867
joseareia <jose.apareia@gmail.com>
2022-06-26 17:07:57 +0100

    edit: global errors

--- src/main/java/vista/oficinas/RegistarOficina.java
+++ src/main/java/vista/oficinas/RegistarOficina.java
@@ -29,6 +29,11 @@ public class RegistarOficina extends JDialog {
     private JTextField textNome;
     private JTextField textNomeResp;
     private JTextField textNumResp;
+    private JLabel lbTelefone;
+    private JLabel lbMail;
+    private JLabel lbNome;
+    private JLabel lbNumResp;
+    private JLabel lbNomeResp;
 
     public RegistarOficina() {
         setContentPane(painelPrincipal);
@@ -42,20 +47,45 @@ public class RegistarOficina extends JDialog {
     }
 
     private void btnRegistarActionPerformed(ActionEvent e) {
-        if (textNome.getText().length() < 2 || textNome.getText().length() > 255) {
-            Erros.mostrarErro(this, 1);
-        }
+        String nome = textNome.getText();
+        String mail = textMail.getText();
+        String nomeResp = textNomeResp.getText();
+        int telefone = (textTelefone.getText().isEmpty() ? -1 : Integer.parseInt(textTelefone.getText()));
+        int telefoneResp = (textNumResp.getText().isEmpty() ? -1 : Integer.parseInt(textNumResp.getText()));
 
-        DadosApp.getInstancia().inserirOficina(
-                new Oficina(
-                        textNome.getText(),
-                        Integer.parseInt(textTelefone.getText()),
-                        textMail.getText(),
-                        textNomeResp.getText(),
-                        Integer.parseInt(textNumResp.getText())
-                )
-        );
-        dispose();
+        if (nome.length() < 2 || nome.length() > 255)
+        {
+            Erros.mostrarErro(this, 1, Erros.removeLastChar(lbNome.getText()));
+        }
+        else if (telefone == -1 || textTelefone.getText().length() != 9)
+        {
+            Erros.mostrarErro(this, 5, Erros.removeLastChar(lbTelefone.getText()));
+        }
+        else if (!Erros.validate(mail))
+        {
+            Erros.mostrarErro(this, 2, Erros.removeLastChar(lbMail.getText()));
+        }
+        else if (nomeResp.length() < 2 || nomeResp.length() > 255)
+        {
+            Erros.mostrarErro(this,1, Erros.removeLastChar(lbNomeResp.getText()));
+        }
+        else if (telefoneResp == -1 || textNumResp.getText().length() != 9)
+        {
+            Erros.mostrarErro(this, 5, Erros.removeLastChar(lbNumResp.getText()));
+        }
+        else
+        {
+            DadosApp.getInstancia().inserirOficina(
+                    new Oficina(
+                            nome,
+                            telefone,
+                            mail,
+                            nomeResp,
+                            telefoneResp
+                    )
+            );
+            dispose();
+        }
     }
 
     private void btnCancelarActionPerformed(ActionEvent e) {
